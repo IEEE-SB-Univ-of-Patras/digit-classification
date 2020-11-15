be4f2247657c63f791138c4225b0ccf52aee21c2
giorgosbaknis <giorgosbaknis@gmail.com>
2020-11-15 18:39:10 +0200

    Update digit classification.py

--- digit classification.py
+++ digit classification.py
@@ -1,1 +1,23 @@
-print("hello")
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import datasets, svm, metrics
+from sklearn.model_selection import train_test_split
+
+#load data
+digits = datasets.load_digits()
+n_samples = len(digits.images)
+data = digits.images.reshape((n_samples, -1))
+
+#classifier
+classifier = svm.SVC(gamma = 0.01)
+
+#split data
+X_train, X_test, y_train, y_test = train_test_split(data, digits.target, test_size=0.5, shuffle=False)
+
+#train
+classifier.fit(X_train, y_train)
+
+predicted = classifier.predict(X_test)
+
+print("Classification report for classifier %s:\n%s\n"
+      % (classifier, metrics.classification_report(y_test, predicted)))
